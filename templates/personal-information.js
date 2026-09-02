c95d8a7f5c63a1ff1ecb0b9e2fa11bc06acffb2e
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:52:50 +0800

    Add editable personal information template

---
+++ templates/personal-information.js
@@ -0,0 +1,2 @@
+window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
+window.LETTER_TEMPLATES.personalInformation = `I am disputing the personal information identified below. Please conduct a reasonable reinvestigation of the inaccurate identifying information and correct or delete any information that cannot be verified as accurate and complete.`;
