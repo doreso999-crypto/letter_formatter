c20bd0a5bddb51f6360947e009737b595e280a74
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:57:10 +0800

    Add dismissed bankruptcy template

---
+++ templates/dismissed-bankruptcy.js
@@ -0,0 +1,2 @@
+window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
+window.LETTER_TEMPLATES.dismissedBankruptcy = 'I am disputing the bankruptcy-related information reported for the account(s) listed below as dismissed. Please verify the disposition and all related reporting, and delete or correct any information that cannot be verified as accurate and complete.';
