a7fc24eefe04ef1b290a7aeb2f6b4d59f75f26a4
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:52:25 +0800

    Add base letter template

---
+++ templates/base.js
@@ -0,0 +1,6 @@
+window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
+window.LETTER_TEMPLATES.base = {
+  subject: 'Re: Dispute of Inaccurate, Fraudulent, or Unverifiable Information',
+  opening: 'I am writing to formally dispute the accuracy and completeness of the information identified below as it appears on my consumer report. I request a reasonable reinvestigation of each listed item and correction or deletion of any information that cannot be verified as accurate and complete.',
+  closing: 'Please investigate each disputed item individually and provide the results of the reinvestigation. Where any information cannot be verified as accurate and complete, please correct or delete the information and provide an updated copy of my consumer report reflecting the results.'
+};
