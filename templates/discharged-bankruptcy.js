0a006905fc7d5f9cd8cd2d3c83a53b452371c50c
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:57:13 +0800

    Add discharged bankruptcy template

---
+++ templates/discharged-bankruptcy.js
@@ -0,0 +1,2 @@
+window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
+window.LETTER_TEMPLATES.dischargedBankruptcy = 'I am disputing the bankruptcy-related information reported for the account(s) listed below as discharged. Please verify the discharge status and all related reporting, and delete or correct any information that cannot be verified as accurate and complete.';
