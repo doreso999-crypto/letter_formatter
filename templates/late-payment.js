9879af2ba7db3f4b56b7380223d950b0df656193
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:57:05 +0800

    Add late payment template

---
+++ templates/late-payment.js
@@ -0,0 +1,2 @@
+window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
+window.LETTER_TEMPLATES.latePayment = 'I am writing to dispute the late-payment information reported for the account(s) listed below. Please reinvestigate the reported payment history and delete any late-payment information that cannot be verified as accurate and complete.';
