718a70fe0894b43891fc2d9c2641244e1da7678f
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:58:24 +0800

    Remove obsolete generic open-closed-late template

--- templates/open-closed-late.js
+++
@@ -1,2 +0,0 @@
-window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
-window.LETTER_TEMPLATES.openClosedLate = `I am disputing the reported open, closed, and/or late-payment information for the account listed below. Please conduct a reasonable reinvestigation of the reported status and payment history and correct or delete any information that cannot be verified as accurate and complete.`;
