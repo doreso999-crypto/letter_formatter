e873279164ec5bd4f3815f9a3be45445d4171225
doreso999-crypto <doreso999@gmail.com>
2026-09-02 21:52:45 +0800

    Add editable hard inquiry template

---
+++ templates/hard-inquiry.js
@@ -0,0 +1,2 @@
+window.LETTER_TEMPLATES = window.LETTER_TEMPLATES || {};
+window.LETTER_TEMPLATES.hardInquiry = `I am disputing the hard inquiry identified below. Please conduct a reasonable reinvestigation of the inquiry, including the reported source, date, and permissible purpose, and correct or delete any information that cannot be verified as accurate and complete.`;
